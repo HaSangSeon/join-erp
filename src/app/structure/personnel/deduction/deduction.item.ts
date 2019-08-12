abd622c48056ea570480e7e74142f01170bea621
kakasiro7 <kakasiro7@naver.com>
2019-08-12 17:29:50 +0900

    deduction.item 추가

---
+++ src/app/structure/personnel/deduction/deduction.item.ts
@@ -0,0 +1,15 @@
+export class Item{
+    data: {
+        deduction_code: string;
+        year: string;
+        acct_code: string;
+        calculation_method: string;
+        entry_seq: string;
+        etc: string;
+        createdAt: string;
+
+    };
+    maxResultCount: number;
+    result: string;
+    errorMessage: string;
+}
