0b49f255b7572b58e4f26733a34bf79fd538b9aa
kakasiro7 <kakasiro7@naver.com>
2019-08-27 14:34:53 +0900

    호봉제임금테이블(payStep) 엑셀업로드 완료

--- src/app/structure/settings/personnel/pay-step/pay-step.component.ts
+++ src/app/structure/settings/personnel/pay-step/pay-step.component.ts
@@ -72,7 +72,9 @@ export class PayStepComponent implements OnInit {
         this.temp = listData['data'];
         this.rows = listData['data'];
 
-        this.ps_job_type = listData['data'][0]['job_type'][0];
+        if(this.ps_job_type!=""){
+          this.ps_job_type = listData['data'][0]['job_type'][0];
+        }
 
         this.isLoadingProgress = false;
       }
@@ -86,46 +88,6 @@ export class PayStepComponent implements OnInit {
     }
 
   }
-  excelDown(): void {
-
-    let ym = this.excelSchYear.nativeElement.value + "-" + this.excelSchMonth.nativeElement.value
-    this.dataService.GetExcelFile(ym).subscribe(
-      blob => {
-        // Filesaver.js 1.3.8
-        // 사용자가 지정한 저장위치를 읽을 수 있는 방법이 없어 저장된 파일의 링크를 제공할 수 없음.
-        importedSaveAs(blob, "시산표.xlsx");
-
-        let win = this.electronService.remote.getCurrentWindow();
-
-        win.webContents.session.on('will-download', (event, item, webContents) => {
-
-          const filename = item.getFilename();
-
-          item.on('updated', (event, state) => {
-            if (state === 'interrupted') {
-              console.log('Download is interrupted but can be resumed')
-            } else if (state === 'progressing') {
-              if (item.isPaused()) {
-                console.log('Download is paused')
-              } else {
-                console.log(`Received bytes: ${item.getReceivedBytes()}`)
-              }
-            }
-          })
-          item.once('done', (event, state) => {
-            if (state === 'completed') {
-              console.log(filename + ' 저장 완료');
-              this.uploadFormModal.hide();
-            } else {
-              alert('저장하려는 파일이 열려져 있습니다. 파일을 닫은 후 다시 진행해주세요');
-              console.log(`Download failed: ${state}`)
-            }
-          })
-        });
-      },
-      error => this.errorMessage = <any>error
-    );
-  }
 
   fileSelected () {
     let fileList: FileList = this.uploadFileSrc.nativeElement.files;
@@ -133,9 +95,6 @@ export class PayStepComponent implements OnInit {
         let file: File = fileList[0];
         let formData:FormData = new FormData();
         formData.append('uploadFile', file, file.name);
-        //test
-        console.log("cc");
-        console.log(formData);
 
         this.excelUpload(formData);
     }
@@ -144,9 +103,6 @@ export class PayStepComponent implements OnInit {
 excelUpload (data): void {
     this.dataService.UploadExcelFile(data).subscribe(
         data => {
-          //test
-          console.log("dd");
-          console.log(data);
 
             if (data['result'] == "success") {
                 this.inputForm.reset();
